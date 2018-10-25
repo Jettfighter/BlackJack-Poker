2a3adadcdeabb845f9cba621a446813ace7ef6bf
RyanKivett <36863659+RyanKivett@users.noreply.github.com>
2018-10-25 13:41:03 -0600

    Models created

---
+++ CardGames/CardGameLib/Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGameLib.Models
+{
+    public class Card
+    {
+        public char Value { get; set; }
+        public string Suit { get; set; }
+
+        public Card(char value, string suit)
+        {
+            this.Value = value;
+            this.Suit = suit;
+        }
+    }
+}
