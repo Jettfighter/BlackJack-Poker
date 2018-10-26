a117e835e977876be8b03a0063002c03f83532f6
Damon <damon.black.dwb@gmail.com>
2018-10-26 14:52:33 -0600

    thing

--- CardGames/CardGameLib/Controllers/BlackJackController.cs
+++ CardGames/CardGameLib/Controllers/BlackJackController.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CardGameLib.Models;
 
 namespace CardGameLib.Controllers
 {
     class BlackJackController
     {
+        BlackJack blackjack;
+
+        public void StartGame(string[] playerNames)
+        {
+
+        }
+
+        public bool TakeTurn(bool hitMe)
+        {
+            bool yes = true;
+
+            return yes;
+        }
+
     }
 }
