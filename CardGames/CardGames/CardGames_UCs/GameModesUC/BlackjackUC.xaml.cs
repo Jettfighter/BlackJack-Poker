3e0e0504728278090ed7f50028e983f5a0b8ca02
Jett Tripp <jtripp@student.neumont.edu>
2018-10-29 14:54:32 -0600

    Added some setup for UI in blackcjack

--- CardGames/CardGames/CardGames_UCs/GameModesUC/BlackjackUC.xaml.cs
+++ CardGames/CardGames/CardGames_UCs/GameModesUC/BlackjackUC.xaml.cs
@@ -20,9 +20,50 @@ namespace CardGames.CardGames_UCs.GameModesUC
     /// </summary>
     public partial class BlackjackUC : UserControl
     {
+        public List<Label> PlayerNameDisplays = new List<Label>();
+
+        public List<Label> PlayerBankDisplays = new List<Label>();
+
+        public List<StackPanel> PlayerHandDisplays = new List<StackPanel>();
+
         public BlackjackUC()
         {
             InitializeComponent();
+        }
+
+        private void UC_Loaded(object sender, RoutedEventArgs e)
+        {
+            PreparePlayerElements();
+            ResetPlayers();
+
+        }
+
+        private void ResetPlayers()
+        {
+            //TODO: Update names
+            //TODO: Update player banks
+            //TODO: Update bet amount - OPTIONAL
+        }
+
+        private void PreparePlayerElements()
+        {
+            PlayerNameDisplays.Add(Player1Name);
+            PlayerNameDisplays.Add(Player2Name);
+            PlayerNameDisplays.Add(Player3Name);
+            PlayerNameDisplays.Add(Player4Name);
+            PlayerNameDisplays.Add(Player5Name);
+
+            PlayerBankDisplays.Add(Player1Bank);
+            PlayerBankDisplays.Add(Player2Bank);
+            PlayerBankDisplays.Add(Player3Bank);
+            PlayerBankDisplays.Add(Player4Bank);
+            PlayerBankDisplays.Add(Player5Bank);
+
+            PlayerHandDisplays.Add(Player1Hand);
+            PlayerHandDisplays.Add(Player2Hand);
+            PlayerHandDisplays.Add(Player3Hand);
+            PlayerHandDisplays.Add(Player4Hand);
+            PlayerHandDisplays.Add(Player5Hand);
         }
     }
 }
