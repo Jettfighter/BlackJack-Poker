f7fb8237542377565dd14f691ff5f34b78c04f70
klimapaige <klima.paige@gmail.com>
2018-10-30 10:30:25 -0600

    Working on blackjack stuff some more

--- CardGames/CardGameLib/Models/BlackjackPlayer.cs
+++ CardGames/CardGameLib/Models/BlackjackPlayer.cs
@@ -13,5 +13,7 @@ namespace CardGameLib.Models
         public int SecondBet { get; set; }
         public bool SecondHandInitiated { get; set; } 
         public bool BoughtIn { get; set; }
+        public bool DoubledDown { get; set; }
+
     }
 }
